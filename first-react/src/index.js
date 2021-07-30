c91d3afbdda2fee25532651eddeccdcad893d374
sujanchhengutala <47767420+sujanchhengutala@users.noreply.github.com>
2021-07-30 15:06:43 +0545

    used map

--- first-react/src/index.js
+++ first-react/src/index.js
@@ -1,41 +1,61 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './index.css'
 
-const Books = {
-  firstBook: {
+const books = [
+  {
+    id :1,
     title: "Ghumne Mechmathi Andho Manche",
     author: "Bhupi Sherchan",
-    image: "https://m.media-amazon.com/images/I/61iJPlzk4wL._AC_UY218_.jpg"
+    img: "https://m.media-amazon.com/images/I/61iJPlzk4wL._AC_UY218_.jpg"
   },
-  secondBook: {
+ {
+    id :2,
     title: "China Harayeko Manchhe",
     author: "Haribansha Acharya ",
-    image: "https://m.media-amazon.com/images/I/71C6wasTqYL._AC_UY218_.jpg"
-  },
-  thirdBook: {
+    img: "https://m.media-amazon.com/images/I/71C6wasTqYL._AC_UY218_.jpg"
+  }, 
+  {
+    id :3,
     title: "Phoolko Aankhaama",
     author: "Ani Choying Drolma",
-    image: "https://m.media-amazon.com/images/I/61-shXcuDAL._AC_UY218_.jpg"
+    img: "https://m.media-amazon.com/images/I/61-shXcuDAL._AC_UY218_.jpg"
   }
-}
+]
 
-const BookList = () => {
+function BookList() {
   return (
-    <section className="booklist">
-      <Book image={Books.firstBook.image} title={Books.firstBook.title} author={Books.firstBook.author} />
-      <Book image={Books.secondBook.image} title={Books.secondBook.title} author={Books.secondBook.author} />
-      <Book image={Books.thirdBook.image} title={Books.thirdBook.title} author={Books.thirdBook.author} />
+    <section className='booklist'>
+      {books.map((book) => (
+        <Book key={book.id} {...book} />
+      ))}
     </section>
   )
 }
-const Book = ({ image, title, author }) => {
+
+const Book = ({ img, title, author }) => {
+  const clickHandler = () => {
+    alert('I am clicked')
+  }
+
+  const exampleHandler = (author) => {
+    console.log(author)
+  }
   return (
-    <article calssName="book">
-      <img src={image} />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+    <article className='book' onMouseOver={() => console.log(title)}>
+      <img src={img} alt='' />
+      <h1 onClick={() => console.log(title)}> {title} </h1>
+      <h4> {author} </h4>
+      <button type='button' onClick={clickHandler}>
+        Click Me
+      </button>
+
+      <button  className = "but" type='button' onClick={() => exampleHandler(author)}>
+        Author Name
+      </button>
     </article>
   )
 }
-ReactDOM.render(<BookList />, document.getElementById('root'));
+
+ReactDOM.render(<BookList />, document.getElementById('root'))
