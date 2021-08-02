cf66c176a80534d8a763cdbad4dfb1574d2fdb5b
sujanchhengutala <47767420+sujanchhengutala@users.noreply.github.com>
2021-08-02 07:41:56 +0545

    ternary operator

--- react-second/src/1-useState/1-useState-Basic.js
+++ react-second/src/1-useState/1-useState-Basic.js
@@ -2,17 +2,13 @@ import React, { useState } from "react";
 const UseStateBasic = () => {
     const [text, setText] = useState('sujan chhengutala')
     const handleClick = () => {
-
-        if (text == 'sujan chhengutala') {
-            setText(`He is react student`)
-        }
-        else {
-            setText('He is not react student')
-        }
+        return (text === 'sujan chhengutala') ? setText('He is react student') : setText('He is not react student')
+        
     }
-    return <>
+    return ( <>
         <h1>{text}</h1>
         <button className="btn" onClick={handleClick} >click me</button>
     </>
+    )
 }
 export default UseStateBasic;
