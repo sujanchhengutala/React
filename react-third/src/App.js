ed51cb51e89b6a77866c4cad3474d81d8c77f83b
sujanchhengutala <47767420+sujanchhengutala@users.noreply.github.com>
2021-08-02 15:51:26 +0545

    linked useState of object in apps

--- react-third/src/App.js
+++ react-third/src/App.js
@@ -1,8 +1,8 @@
-import ArrayState from './1-useState/2-useState-Array';
+import ObjectState from './1-useState/3-useState-object';
 function App() {
   return (
     <div className="container">
-     <ArrayState />
+     <ObjectState />
     </div>
   );
 }
