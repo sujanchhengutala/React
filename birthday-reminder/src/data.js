0b60bf01b86dff025d3717e5e7820bd788e55d81
sujanchhengutala <47767420+sujanchhengutala@users.noreply.github.com>
2021-08-04 12:48:18 +0545

    Birthday remainder

---
+++ birthday-reminder/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Sujan chhengutala',
+    age: 22,
+    image:
+      'https://media-exp1.licdn.com/dms/image/C5103AQG-WakRatSq_Q/profile-displayphoto-shrink_200_200/0/1536243325270?e=1633564800&v=beta&t=JZXTqGxaxkoQ9_-GjAyfG70cb3QErp8jycBCeihmO3g',
+  },
+  {
+    id: 2,
+    name: 'Bishal pilla shrestha',
+    age: 24,
+    image:
+      'https://imgv2-2-f.scribdassets.com/img/word_user/527830018/72x72/0c251a4732/1627979520?v=1',
+  },
+  {
+    id: 3,
+    name: 'Bidur Parajuli',
+    age: 36,
+    image:
+      'https://avatars.githubusercontent.com/u/83571599?v=4',
+  },
+  {
+    id: 4,
+    name: 'Sanjit Katwal',
+    age: 26,
+    image:
+      'https://avatars.githubusercontent.com/u/6796470?v=4',
+  },
+  {
+    id: 5,
+    name: 'Binaya Maharjan',
+    age: 25,
+    image:
+      'https://avatars.githubusercontent.com/u/45600506?v=4',
+  },
+];
