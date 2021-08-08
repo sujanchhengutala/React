b3bba504feee7b51e8757e3228318e5a8e591926
sujanchhengutala <47767420+sujanchhengutala@users.noreply.github.com>
2021-08-08 19:41:04 +0545

    number of state in form reduced to only one

--- react-third/src/4-reactForm-Basic/1-reactForm.js
+++ react-third/src/4-reactForm-Basic/1-reactForm.js
@@ -1,37 +1,54 @@
 import React, { useState } from 'react'
 const ReactForm = () => {
-    const [firstName, setFirstName] = useState('')
-    const [email, setEmail] = useState('')
+    // const [firstName, setFirstName] = useState('')
+    // const [email, setEmail] = useState('')
+    // const [age, setAge] = useState('')
+    const [person, setPerson]= useState({firstName: '', email: '', age: ''})
     const [people, setPeople] = useState([])
     console.log(people)
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const person = { id: new Date().getTime(), firstName, email }
-        if (firstName && email) {
-            setPeople((People) => {
-                return [...People, person]
-            })
-            setFirstName('')
-            setEmail('')
-        }
-        else {
-            console.log('your value is empty')
-        }
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     const person = { id: new Date().getTime(), firstName, email }
+    //     if (firstName && email) {
+    //         setPeople((People) => {
+    //             return [...People, person]
+    //         })
+    //         setFirstName('')
+    //         setEmail('')
+    //     }
+    //     else {
+    //         console.log('your value is empty')
+    //     }
+    // }
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        if (person.firstName && person.email && person.age) {
+                    const newPerson = {...person, id: new Date().getTime().toString()}
+                    setPeople([...people, newPerson])
+                    setPerson({firstName: '', email: '', age: ''})
+            }
+
+    }
+    const handleChange = (e)=>{
+        const name = e.target.name
+        const value = e.target.value
+        setPerson({...person, [name]: value})
+
     }
     return (
         <>
             <article className="container">
                 <h1>React form basic</h1>
-                <form className="form" onSubmit={handleSubmit}>
+                <form className="form">
                     <div className="form-control">
                         <label htmlFor="firstName">Name :</label>
                         <input
                             type="text"
                             id="firstName"
                             name='firstName'
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)} />
+                            value={person.firstName}
+                            onChange={handleChange} />
                     </div>
                     <div className="form-control">
                         <label htmlFor="email">Email :</label>
@@ -39,17 +56,27 @@ const ReactForm = () => {
                             type="text"
                             id="email"
                             name='email'
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)} />
+                            value={person.email}
+                            onChange={handleChange} />
                     </div>
-                    <button type='submit'> Add Person </button>
+                    <div className="form-control">
+                        <label htmlFor="age">Age :</label>
+                        <input
+                            type="text"
+                            id="age"
+                            name='age'
+                            value={person.age}
+                            onChange={handleChange} />
+                    </div>
+                    <button type='submit' onClick= {handleSubmit}> Add Person </button>
                 </form>
                 {
                     people.map((person) => {
-                        const { id, firstName, email } = person;
+                        const { id, firstName, email, age } = person;
                         return <div className='item' key={id}>
                             <h4>{firstName}</h4>
-                            <p>{email}</p>
+                            <h6>{email}</h6>
+                            <h6>{age}</h6>
                         </div>
                     })
                 }
