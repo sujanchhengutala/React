4decb44f675c1358cc0b0822e85c0ec17ce49abf
sujanchhengutala <47767420+sujanchhengutala@users.noreply.github.com>
2021-08-23 19:19:02 +0545

    react-optimization final

--- reducer-hook/src/6-react-optimization/index.js
+++ reducer-hook/src/6-react-optimization/index.js
@@ -1,21 +1,26 @@
-import React, {useState} from 'react'
+import React, {useCallback, useState} from 'react'
 import { useFetch } from '../4-custom-hook/2-useFetch'
 const url = 'https://reactcourseapi.gorakhjoshi.com/';
 
 const Index = () => {
     const { products } = useFetch(url)
     const [count, setCount] = useState(0)
+   const[cart, setCart] = useState(0)
     console.log('Index compoent is running')
     // console.log(products)
+    const addToCart =useCallback(()=>{
+        setCart(cart + 1)
+    },[cart])
     return (
         <>
         <h1>{count}</h1>
         <button className = 'btn' onClick = {() => setCount(count + 1)}>counter </button>
-            <BigList products={products} />
+        <h2 style = {{marginTop : '3rem'}}>Cart: {cart}</h2>
+            <BigList products={products} addToCart = {addToCart} />
         </>
     )
 }
-    const BigList = React.memo(({ products}) => {
+    const BigList = React.memo(({ products, addToCart}) => {
         // console.log(products)
         console.log('BigList is running')
 
@@ -23,7 +28,7 @@ const Index = () => {
         <>
             {products.map((product) => {
                 // console.log(product.id)
-                return <SingleProducts key={product.id} {...product} ></SingleProducts>
+                return <SingleProducts key={product.id} {...product} addToCart = {addToCart} ></SingleProducts>
 
             })}
 
@@ -31,7 +36,7 @@ const Index = () => {
 
     )
 })
-const SingleProducts = ({fields})=>{
+const SingleProducts = ({fields, addToCart})=>{
     console.log('product is running')
     const {name, price} = fields
     // console.log(fields.name)
@@ -39,6 +44,7 @@ const SingleProducts = ({fields})=>{
     <img src = {fields.image[0].url} alt = '' />
     <h4>{name}</h4>
     <p>{price}</p>
+    <button onClick = {addToCart}>Add to cart</button>
     </article>
 }
 
